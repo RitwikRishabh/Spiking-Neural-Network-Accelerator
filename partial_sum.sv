55108ad2e9406071bb73a2c75d35e1e91c720ca6
Trinath Harikrishna <98401167+htrinath@users.noreply.github.com>
2023-04-26 14:56:15 -0700

    Update partial_sum.sv

--- partial_sum.sv
+++ partial_sum.sv
@@ -21,7 +21,7 @@ module partial_sum (interface in, interface out);
     localparam outSpikeType = 2'b11;
 
     logic [WIDTH-1:0] inputPacket = 0;
-    reg [WIDTH_MEMBRANE_POTENTIAL-1:0] partialPE1[2:0], partialPE2[2:0], partialPE3[2:0], partialPE4[2:0], partialPE5[2:0];
+    reg [WIDTH_MEMBRANE_POTENTIAL-1:0] partialPE1[3:0], partialPE2[3:0], partialPE3[3:0], partialPE4[3:0], partialPE5[3:0];
     logic outputSpike;
     logic [WIDTH-1:0] outputPacket;
     logic [9:0] outputSpikeAddr;
@@ -30,6 +30,7 @@ module partial_sum (interface in, interface out);
     logic [12:0] residue_mem[2:0][20:0];
     integer i=0,j=0;
     logic [4:0] count_X = 0, count_Y = 0;
+    logic [4:0] y_addr = 0;
     integer counts = 0;
     int count1=0, count2=0, count3=0, count4=0, count5=0;
 
@@ -151,7 +152,10 @@ module partial_sum (interface in, interface out);
             // outputPacket = {ADDER_ADDR, addrMem, membranePotType, membraneToMemZeroes, membranePotential};
             // out.Send(outputPacket);
             if (outputSpike) begin
-                outputSpikeAddr = {count_X, (count_Y*7) + ADDER_NUM};
+                y_addr = (count_Y*7) + ADDER_NUM;
+                outputSpikeAddr = {count_X, y_addr};
+                // $display("COUNT_X:::%b, COUNT_Y::::%b,  %b",count_X, count_Y,{count_X,count_Y});
+                // $display("OUTPUTSPIKEADDR:::::%b", outputSpikeAddr);
                 outputPacket = {DESTINATION_ADDRESS, SOURCE_ADDRESS, outSpikeType, outToMemZeroes, outputSpikeAddr};
                 out.Send(outputPacket);
                 $display(" %m SENT OUTPUT PACKET :::::: %h", outputPacket);
@@ -180,3 +184,98 @@ module partial_sum (interface in, interface out);
         
     end
 endmodule
+
+
+///////////////////////////testbench///////////////////
+
+module data_generator (interface r);
+  parameter FL = 0;
+  //parameter SENDVALUE = 32'h1111_1111;
+  parameter PS_ADDRESS = 4'b0000;
+  parameter WIDTH = 64;
+  logic [WIDTH-1:0] SendValue=0;
+  integer source_address=0, i=0;
+  logic [3:0] addr_array[4:0] = {4'b0001, 4'b0101, 4'b0011, 4'b0111, 4'b1100};
+
+
+  always
+  begin 
+    
+	//add a display here to see when this module starts its main loop
+    //$display("*** %m %d",$time);
+	
+    source_address = i % 5; // the range of random number is from 0 to 2^WIDTH
+    $display("SOURCE ADDRESS :::: %d", source_address);
+    $display("Address_Array:::%b",addr_array[source_address]);
+    SendValue = {PS_ADDRESS, addr_array[source_address], 48'd0, 8'd20};
+    #FL;   // change FL and check the change of performance
+     
+    //Communication action Send is about to start
+    //$display("Start sending in module %m. Simulation time =%t", $time);
+    r.Send(SendValue);
+    $display("SENDING VALUE::::%h", SendValue);
+	i+=1;
+    //Communication action Send is finished
+    //$display("Finished sending in module %m. Simulation time =%t", $time);
+	
+
+  end
+endmodule
+
+//Sample data_bucket module
+module data_bucket (interface r);
+  parameter WIDTH = 64;
+  parameter BL = 0; //ideal environment    backward delay
+  logic [WIDTH-1:0] ReceiveValue = 0;
+  
+  //Variables added for performance measurements
+  real cycleCounter=0, //# of cycles = Total number of times a value is received
+       timeOfReceive=0, //Simulation time of the latest Receive 
+       cycleTime=0; // time difference between the last two receives
+  real averageThroughput=0, averageCycleTime=0, sumOfCycleTimes=0;
+  always
+  begin
+	
+	//add a display here to see when this module starts its main loop
+  //$display("*** %m %d",$time);
+
+    timeOfReceive = $time;
+	
+	//Communication action Receive is about to start
+	//$display("Start receiving in module %m. Simulation time =%t", $time);
+    r.Receive(ReceiveValue);
+
+    //$display("Received Data: %d ------ %b", ReceiveValue, ReceiveValue);
+    $display("\nPacket Received at DATA BUCKET %m and Packet value: %h\n", ReceiveValue);
+	
+	//Communication action Receive is finished
+  //$display("Finished receiving in module %m. Simulation time =%t", $time);
+
+	#BL;
+    cycleCounter += 1;		
+    //Measuring throughput: calculate the number of Receives per unit of time  
+    //CycleTime stores the time it takes from the begining to the end of the always block
+    cycleTime = $time - timeOfReceive; // the difference of time between now and the last receive
+    averageThroughput = cycleCounter/$time; 
+    sumOfCycleTimes += cycleTime;
+    averageCycleTime = sumOfCycleTimes / cycleCounter;
+    //$display("Execution cycle= %d, Cycle Time= %d, 
+    //Average CycleTime=%f, Average Throughput=%f", cycleCounter, cycleTime, 
+    //averageCycleTime, averageThroughput);
+	
+	
+  end
+
+endmodule
+
+
+module partial_sum_tb;
+
+Channel #(.hsProtocol(P4PhaseBD), .WIDTH(64)) intf [1:0] ();
+
+data_generator #(.PS_ADDRESS(4'b0000)) dg (intf[0]);
+partial_sum #(.ADDER_NUM(5'd0)) ps (intf[0], intf[1]);
+data_bucket db (intf[1]);
+
+endmodule
+
